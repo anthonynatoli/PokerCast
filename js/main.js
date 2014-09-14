250b2ab369a6e95195b740d17a406ac079dce506
Ben Wencke <benwencke@gmail.com>
2014-09-13 21:59:18 -0400

    Added example "join" command

--- js/main.js
+++ js/main.js
@@ -2,4 +2,9 @@ $(CastFramework).ready(function() {
     CastFramework.start('urn:x-cast:com.pokercast.receiver');
     $(document.body).css("background-color", "#1693A5");
     $('#status').text("it's working! yay!");
+
+    $(CastFramework).on("join", function(event, clientId, content) {
+	 	CastFramework.sendMessage(clientId, "exampleCommand", "hello!");
+	 });
+
 });
