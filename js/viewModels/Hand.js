41fb447d4c38857097a447e89de0bc9a1dbd67e9
Ben Wencke <benwencke@gmail.com>
2014-09-25 22:29:51 -0400

    Changed initial round value to 0

--- js/viewModels/Hand.js
+++ js/viewModels/Hand.js
@@ -3,7 +3,7 @@ function Hand(deck, chipsPerPlayer, pot) {
 	self.deck = ko.observable(deck || []);
 	self.chipsPerPlayer = chipsPerPlayer || 50;
 	self.pot = ko.observable(pot || 0);
-	self.round = 1;
+	self.round = 0;
 	self.cardsOnTable = ko.observableArray([]);
 	self.currentPlayer = ko.observable();
 }
