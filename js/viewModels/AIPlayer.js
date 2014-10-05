9350aab3163efd3a3b2721f9dc5ccda4dac779d0
jkoncel <joekoncel@yahoo.com>
2014-10-05 14:38:34 -0400

    debugging 2

--- js/viewModels/AIPlayer.js
+++ js/viewModels/AIPlayer.js
@@ -2,7 +2,7 @@ function AIPlayer(id, chips) {
 	Player.apply(this, ['aiPlayer'+id, names.splice(Math.floor(Math.random()*names.length),1)[0]])
 	var self = this;
 	var savedChips = chips;
-	var handEval = Math.Random()*10;
+	var handEval = 2; //Math.Random()*10;
 	//var raised = false;
 	self.type = "AIPlayer";
 	self.makeBet = function(bet) {
