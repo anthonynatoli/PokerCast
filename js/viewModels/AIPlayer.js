d308ac61704ef76081d7401fbf2419fe2535bd5a
jkoncel <joekoncel@yahoo.com>
2014-10-16 23:41:45 -0400

    Defect: Bubba will never be selected as an AI player

--- js/viewModels/AIPlayer.js
+++ js/viewModels/AIPlayer.js
@@ -1,5 +1,5 @@
 function AIPlayer(id, chips) {
-	Player.apply(this, ['aiPlayer'+id, names.splice(Math.floor(Math.random()*names.length),1)[0]])
+	Player.apply(this, ['aiPlayer'+id, names.splice(Math.floor(Math.random()*names.length-1),1)[0]])
 	var self = this;
 	var savedChips = chips;
 	self.handEval = -1;
